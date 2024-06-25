a6ee057a32a8ddd4501c9dbb8b9ace94cda5021b
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-26 02:54:47 +0800

    Create subsample_medalign_ft1_ds.py

---
+++ dataset_prep/subsample_medalign_ft1_ds.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset, Dataset
+
+
+def load_finetune_1():
+    train_ds: Dataset = load_dataset("lemousehunter/med-instruct-align-1_parquet")['train']
+    assert train_ds.column_names == ["input", "output", "system"]
+
+    train_1_ds, train_2_ds = train_ds.train_test_split(test_size=0.04)
+
+    train_1_ds.push_to_hub("lemousehunter/med-instruct-align-1_train_293K")
+    train_2_ds.push_to_hub("lemousehunter/med-instruct-align-1_train_7.6M")
+
+
+if __name__ == "__main__":
+    load_finetune_1()
