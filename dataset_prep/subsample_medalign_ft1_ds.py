6bda0b3458691416c5f48d8d118c7f3366974c4c
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-26 02:56:39 +0800

    Update subsample_medalign_ft1_ds.py

--- dataset_prep/subsample_medalign_ft1_ds.py
+++ dataset_prep/subsample_medalign_ft1_ds.py
@@ -5,7 +5,9 @@ def load_finetune_1():
     train_ds: Dataset = load_dataset("lemousehunter/med-instruct-align-1_parquet")['train']
     assert train_ds.column_names == ["input", "output", "system"]
 
-    train_1_ds, train_2_ds = train_ds.train_test_split(test_size=0.04)
+    split = train_ds.train_test_split(test_size=0.04)
+    train_1_ds = split['train']
+    train_2_ds = split['test']
 
     train_1_ds.push_to_hub("lemousehunter/med-instruct-align-1_train_293K")
     train_2_ds.push_to_hub("lemousehunter/med-instruct-align-1_train_7.6M")
