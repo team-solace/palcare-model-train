18691f23209f76cdf58dcaf0a6c7146804adfc85
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-25 17:42:36 +0800

    update train script and reqs

--- train/run_train.sh
+++ train/run_train.sh
@@ -1,1 +1,3 @@
-accelerate launch --config_file "palcare-model-train/train/configs/deepspeed/deepspeed_1.yaml" palcare-model-train/train/train_sft.py
+# Trained checkpoints is saved to checkpoints
+mkdir -p checkpoints
+accelerate launch --config_file "palcare-model-train/train/configs/deepspeed/deepspeed_1.yaml" palcare-model-train/train/train_sft.py checkpoints
