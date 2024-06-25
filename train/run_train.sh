043c9c27378ce33d44f3a3e1141732ab9904fca2
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-25 21:59:56 +0800

    debug_deepspeed

--- train/run_train.sh
+++ train/run_train.sh
@@ -1,3 +1,3 @@
 # Trained checkpoints is saved to checkpoints
 mkdir -p checkpoints
-accelerate launch --config_file "palcare-model-train/train/configs/deepspeed/deepspeed_1.yaml" --num_processes 2 palcare-model-train/train/train_sft.py checkpoints
+accelerate launch --config_file "palcare-model-train/train/configs/deepspeed/default_config.yaml" --num_processes 2 palcare-model-train/train/train_sft.py checkpoints
