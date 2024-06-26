06d8ae7e79c08dbc57c40973cb5ce4c1870df38e
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-27 01:48:38 +0800

    Update train.sh

--- litgpt_train/train.sh
+++ litgpt_train/train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-litgpt finetune_lora --data MedInstructAlign --logger_name wandb lemousehunter/epfllm-meditron-7b-base
+litgpt download lemousehunter/epflllm_meditron-7b-base
+wandb login ${WANDB_API_KEY}
+litgpt finetune_lora --devices 2 --data MedInstructAlign --logger_name wandb checkpoints/lemousehunter/epflllm_meditron-7b-base
