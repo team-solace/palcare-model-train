0a373290f6651fcc16a42fa15e3d558a160e527d
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-07-01 19:55:23 +0800

    Update pth_to_safetensors.py

--- convert/pth_to_safetensors.py
+++ convert/pth_to_safetensors.py
@@ -1,7 +1,7 @@
 import os
 
 import torch
-from transformers import AutoModel
+from transformers import LlamaForCausalLM
 from jsonargparse import CLI
 from shutil import copyfile
 
@@ -13,16 +13,18 @@ def convert_model(pytorch_fpath: str, lit_model_path: str, base_model_hf_repo_id
     for f in files_list:
         print("copying", f, "to", output_fpath)
         copyfile(os.path.join(lit_model_path, f), os.path.join(output_fpath, f))
+
     print("=====================================")
     print("loading state dict from", pytorch_fpath)
     state_dict = torch.load(pytorch_fpath)
     print("=====================================")
     print("loading base model from", base_model_hf_repo_id)
-    model = AutoModel.from_pretrained(base_model_hf_repo_id, state_dict=state_dict)
+    model = LlamaForCausalLM.from_pretrained(base_model_hf_repo_id, state_dict=state_dict)
+
     print("=====================================")
     os.makedirs(output_fpath, exist_ok=True)
     print("saving to output_fpath", output_fpath)
-    model.save_pretrained(output_fpath)
+    model.save_pretrained(output_fpath, safe_serialization=False)
 
 
 if __name__ == "__main__":
